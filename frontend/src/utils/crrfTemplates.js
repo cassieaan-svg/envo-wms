e46f7be6b88ada4231ac26022ca940b7a67bd2fd
Christy-Annie  Bassey <chbassey@ecews.org>
2026-07-18 10:47:33 +0100

    CRRF: template lists (ARV/OI, Condom, CD4, RTKs/DBS) as data
    Transcribes the fixed, ordered drug/reagent rows of each national CRRF variant
    (from the government workbooks) into CRRF_TEMPLATES — the list every CRRF must
    print in full, with zeros where the facility has no data. Foundation for the
    CRRF print builders + page reworks (pharmacy ARV/Condom filter, lab CD4/RTKs
    filter) to follow.

---
+++ frontend/src/utils/crrfTemplates.js
@@ -0,0 +1,418 @@
+// National CRRF template lists — the fixed, ordered drug/reagent rows each
+// CRRF variant must print IN FULL (zeros where the facility has no data).
+// Generated from the government CRRF workbooks; do not hand-edit row order.
+export const CRRF_TEMPLATES = {
+  "arv": [
+    {
+      "group": "Fixed Dose Combination Adult first line"
+    },
+    {
+      "name": "AZT/3TC/NVP (300/150/200 mg)",
+      "unit": "60 tabs"
+    },
+    {
+      "name": "ABC/3TC (600/300mg)",
+      "unit": "30 tabs"
+    },
+    {
+      "name": "AZT/3TC (300/150 mg)",
+      "unit": "60 tabs"
+    },
+    {
+      "name": "TDF/3TC(300/300 mg)",
+      "unit": "30 tabs"
+    },
+    {
+      "name": "TDF/3TC/EFV (300/300/600 mg)",
+      "unit": "30 tabs"
+    },
+    {
+      "name": "TDF/3TC/DTG (300/300/50mg)",
+      "unit": "30 tabs"
+    },
+    {
+      "name": "TDF/3TC/DtG(300/300/50mg)",
+      "unit": "90 tab"
+    },
+    {
+      "group": "Single Dose Adult first line"
+    },
+    {
+      "name": "Dolutegravir DTG 50mg",
+      "unit": "30 tabs"
+    },
+    {
+      "name": "Efavirenz EFV 600 mg",
+      "unit": "30 tabs"
+    },
+    {
+      "name": "Zidovudine AZT 300mg",
+      "unit": "60 tabs"
+    },
+    {
+      "name": "Nevirapine NVP 200 mg",
+      "unit": "60 tabs"
+    },
+    {
+      "name": "Dolutegravir DTG 10mg",
+      "unit": "60 tabs"
+    },
+    {
+      "group": "Adult Second Line Drugs"
+    },
+    {
+      "name": "Atazanavir/Ritonavir (ATV/r) 300/100mg",
+      "unit": "30 tabs"
+    },
+    {
+      "name": "Lopinavir/Ritonavir (LPV/r) 200/50mg",
+      "unit": "120 tabs"
+    },
+    {
+      "group": "Adult Third Line Drugs"
+    },
+    {
+      "name": "Darunavir (DRV) 600mg",
+      "unit": "30 tabs"
+    },
+    {
+      "name": "Etravirine 100mg",
+      "unit": "60 tabs"
+    },
+    {
+      "name": "Ritonavir (r) 100mg",
+      "unit": "120 tabs"
+    },
+    {
+      "group": "Pediatric Fixed Dose Combination"
+    },
+    {
+      "name": "ABC/3TC/DTG (60/30/5mg)",
+      "unit": "180 tabs"
+    },
+    {
+      "name": "ABC/3TC ( 120/60mg )",
+      "unit": "30 tabs"
+    },
+    {
+      "name": "AZT/3TC (60/30mg)",
+      "unit": "60 tabs"
+    },
+    {
+      "name": "AZT/3TC/(120/60mg)",
+      "unit": "30 tabs"
+    },
+    {
+      "name": "LoPinavire pellet (40/10mg)",
+      "unit": "120tab"
+    },
+    {
+      "group": "Pediatric Single Dose"
+    },
+    {
+      "name": "DTG 10mg",
+      "unit": "90 tabs"
+    },
+    {
+      "name": "Efavirenz EFV 200mg Scored",
+      "unit": "90 scr tabs"
+    },
+    {
+      "name": "Nevirapine NVP 50mg/5mL",
+      "unit": "25 mL"
+    },
+    {
+      "name": "Nevirapine NVP 50mg/5mL",
+      "unit": "100 mL"
+    },
+    {
+      "name": "Zidovudine AZT 50mg/5mL",
+      "unit": "100 mL"
+    },
+    {
+      "name": "Zidovudine AZT 50mg/5mL",
+      "unit": "240 mL"
+    },
+    {
+      "group": "Pediatric Second Line Drugs"
+    },
+    {
+      "name": "Lopinavir/Ritonavir (LPV/r) 100/25mg",
+      "unit": "60 tabs"
+    },
+    {
+      "name": "Lopinavir/Ritonavir (LPV/r) 40/10mg",
+      "unit": "120 tabs"
+    },
+    {
+      "name": "Raltegravir 25mg",
+      "unit": "60 tabs"
+    },
+    {
+      "name": "Opportunistics Infections Drugs"
+    },
+    {
+      "name": "Co-trimoxazole 120mg",
+      "unit": "tabs"
+    },
+    {
+      "name": "Co-trimoxazole 480mg",
+      "unit": "tabs"
+    },
+    {
+      "name": "Co-trimoxazole 960mg",
+      "unit": "tabs"
+    },
+    {
+      "name": "Fluconazole Oral Suspension 50mg/5ml",
+      "unit": "35ml"
+    },
+    {
+      "name": "Fluonazole 100mg",
+      "unit": "tabs"
+    },
+    {
+      "name": "Fluonazole 200mg",
+      "unit": "tab"
+    },
+    {
+      "name": "Isoniazid (INH) 100mg",
+      "unit": "kits"
+    },
+    {
+      "name": "Isoniazid (INH) 300mg",
+      "unit": "kits"
+    },
+    {
+      "name": "Nystatin Oral Suspension 100,000 IU/ml",
+      "unit": "30ml"
+    },
+    {
+      "name": "Pyridoxine 50mg",
+      "unit": "tabs"
+    },
+    {
+      "name": "ABC/3TC (120/60mg)",
+      "unit": "60 tabs"
+    }
+  ],
+  "condom": [
+    {
+      "name": "Male Condom",
+      "unit": "1 piece"
+    },
+    {
+      "name": "Female condom",
+      "unit": "1 piece"
+    },
+    {
+      "name": "Co-packed condoms and Lubricant",
+      "unit": "1 mini-pack"
+    },
+    {
+      "name": "Lubricant",
+      "unit": "1 piece"
+    }
+  ],
+  "cd4": [
+    {
+      "group": "BD FACSPRESTO REAGENTS"
+    },
+    {
+      "name": "BD FACSPresto Cartridges (100 tests)",
+      "unit": "Test",
+      "pack": "100 Tests"
+    },
+    {
+      "name": "BD FACSPresto Kit. Incl 100 finger stick sample collection kit (lancets, alcohol pad etc)",
+      "unit": "Test",
+      "pack": "100 Tests"
+    },
+    {
+      "name": "Thermal paper roll 5 units (rolls)",
+      "unit": "Roll",
+      "pack": "5 Rolls"
+    },
+    {
+      "group": "PARTEC CYFLOW REAGENTS"
+    },
+    {
+      "name": "Partec CyFlow, CD4 Easy Count Kit, Absolute, 100 Tests",
+      "unit": "1 pack",
+      "pack": "1 bottle (2mls)"
+    },
+    {
+      "name": "Partec CyFlow CD4% Easy Count Kit (PE/PE-Dy647), 100 Tests",
+      "unit": "1 pack",
+      "pack": "2 bottle (1mls)"
+    },
+    {
+      "name": "Partec CyFlow, CountCheck Beads, Green, 50 Tests",
+      "unit": "1 pack",
+      "pack": "2x25ml"
+    },
+    {
+      "name": "Partec CyFlow, Cleaning Solution",
+      "unit": "1 bottle",
+      "pack": "250ml"
+    },
+    {
+      "name": "Partec CyFlow, Decontamination Solution",
+      "unit": "1 bottle",
+      "pack": "250ml"
+    },
+    {
+      "name": "Partec CyFlow, Sheath Fluid",
+      "unit": "1 bottle",
+      "pack": "5 Litres"
+    },
+    {
+      "name": "Partec CyFlow, Sample Tubes, 3.5mL",
+      "unit": "1 pack",
+      "pack": "500pcs"
+    },
+    {
+      "name": "Partec CyFlow, Thermal Printer Paper",
+      "unit": "1 Roll",
+      "pack": "5 Rolls"
+    },
+    {
+      "group": "CD4 Lateral Flow Assay"
+    },
+    {
+      "name": "VISITECT CD4 Advanced disease",
+      "unit": "1 Tests",
+      "pack": "25 tests"
+    },
+    {
+      "group": "PIMA Point of Care CD4 Device"
+    },
+    {
+      "name": "Pima CD4 Catridge Kit,100 Tests",
+      "unit": "1 Test",
+      "pack": "100 Catridges"
+    },
+    {
+      "name": "Pima Bead Standard (1 Normal Catridge, 1 low Catridge)",
+      "unit": "1 pack",
+      "pack": "2 Catridges (Low&Normal)"
+    },
+    {
+      "name": "Pima Sample Collection Kit",
+      "unit": "1 pack",
+      "pack": "100 units"
+    },
+    {
+      "name": "Pima Analyzer Thermal Printing Paper",
+      "unit": "1 Roll",
+      "pack": "10 Rolls"
+    }
+  ],
+  "rtk": [
+    {
+      "group": "HIV RTKs"
+    },
+    {
+      "name": "DETERMINE",
+      "unit": "Test"
+    },
+    {
+      "name": "UNIGOLD",
+      "unit": "Test"
+    },
+    {
+      "name": "STAT-PAK",
+      "unit": "Test"
+    },
+    {
+      "name": "WONDFO",
+      "unit": "Test"
+    },
+    {
+      "name": "CARESTART",
+      "unit": "Test"
+    },
+    {
+      "name": "HIV STATUS",
+      "unit": "Test"
+    },
+    {
+      "name": "FIRST RESPONSE",
+      "unit": "Test"
+    },
+    {
+      "name": "CAREUS",
+      "unit": "Test"
+    },
+    {
+      "name": "MULTISURE",
+      "unit": "Test"
+    },
+    {
+      "name": "DIAQUICK",
+      "unit": "Test"
+    },
+    {
+      "name": "WANTAI",
+      "unit": "Test"
+    },
+    {
+      "name": "STANDARD Q",
+      "unit": "Test"
+    },
+    {
+      "name": "HIV SELF TEST (ORAL BASED)",
+      "unit": "Test"
+    },
+    {
+      "name": "HIV SELF TEST (BLOOD BASED)",
+      "unit": "Test"
+    },
+    {
+      "group": "DBS TEST COLLECTION KITS"
+    },
+    {
+      "name": "DBS TEST COLLECTION BUNDLES",
+      "unit": "Test"
+    },
+    {
+      "group": "Other Rapid kits and OIs"
+    },
+    {
+      "name": "Cryptococcal Antigen Lateral Flow Assay (CrAg- LFA)",
+      "unit": "Test"
+    },
+    {
+      "name": "HIV syphilis dual test kit",
+      "unit": "Test"
+    },
+    {
+      "name": "Recency Test Kit (Asante)",
+      "unit": "Test"
+    },
+    {
+      "name": "Urine TB LF-LAM",
+      "unit": "Test"
+    },
+    {
+      "name": "Hepatitis B",
+      "unit": "Test"
+    },
+    {
+      "name": "Hepatitis C",
+      "unit": "Test"
+    },
+    {
+      "name": "Pregnancy test kit",
+      "unit": "Test"
+    },
+    {
+      "name": "Urinalysis",
+      "unit": "Test"
+    },
+    {
+      "name": "VDRL kit",
+      "unit": "Test"
+    }
+  ]
+}
